4bc985e7883ab12d8face91777019bba962bd9c7
pkarthikbit <pkarthikbit@gmail.com>
2024-11-25 20:10:22 +0530

    LED lights only when temp exceeds the limits

--- main/main.c
+++ main/main.c
@@ -16,20 +16,28 @@
 #include "driver/gpio.h"
 #include "driver/temperature_sensor.h"
 
-/* Variable definition */
-static const char *TAG = "example";
-
 /* constant definition */
-#define BLINK_GPIO GPIO_NUM_15
+#define BLINK_GPIO  GPIO_NUM_15
+#define TIME_1000MS 1000u        
+
+/* Variable definition */
+static const char *TAG = "esp32-remoteCar";     /* for module name logging */
+xTimeOutType x_timeout;                         /* for time out function */
+portTickType openTimeout;                       /* for time out funciton */
+temperature_sensor_handle_t temp_sensor = NULL;
+temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(10, 50);
+float tsens_value;
+bool led_st;
 
 /* Funciton declaration */
 IRAM_ATTR static bool temp_sensor_monitor_cbs(temperature_sensor_handle_t tsens, const temperature_sensor_threshold_event_data_t *edata, void *user_data)
 {
-    ESP_DRAM_LOGI("tsens", "Temperature value is higher or lower than threshold, value is %d\n...\n\n", edata->celsius_value);
+    ESP_DRAM_LOGI(TAG, "Temperature value is higher or lower than threshold, value is %d\n...\n\n", edata->celsius_value);
+    led_st = true;
     return false;
 }
 
-static void blink_task(void *pvParameter)
+static void esp32_remotecar_init(void *pvParameter)
 {
     /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
     muxed to GPIO on reset already, but some default to other
@@ -38,58 +46,60 @@ static void blink_task(void *pvParameter)
     functions.)
     */
     gpio_reset_pin(BLINK_GPIO);
+
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
-    while(1)
-    {
-        /* Blink off (output low) */
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-}
+    vTaskSetTimeOutState(&x_timeout);
+    openTimeout = TIME_1000MS; /*ms*/
 
-static void Tsensor_task(void *pvParameter)
-{
-    ESP_LOGI(TAG, "Install temperature sensor, expected temp ranger range: 10~50 ℃");
-    temperature_sensor_handle_t temp_sensor = NULL;
-    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(10, 50);
     ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor_config, &temp_sensor));
 
     temperature_sensor_event_callbacks_t cbs = {
-        .on_threshold = temp_sensor_monitor_cbs,
+    .on_threshold = temp_sensor_monitor_cbs,
     };
 
     temperature_sensor_abs_threshold_config_t threshold_cfg = {
-    .high_threshold = 50,
-    .low_threshold = -10,
+    .high_threshold = 20,
+    .low_threshold = 16,
     };
 
     ESP_ERROR_CHECK(temperature_sensor_set_absolute_threshold(temp_sensor, &threshold_cfg));
 
     ESP_ERROR_CHECK(temperature_sensor_register_callbacks(temp_sensor, &cbs, NULL));
 
-
-    ESP_LOGI(TAG, "Enable temperature sensor");
+    /* Enable temperature sensor */ 
     ESP_ERROR_CHECK(temperature_sensor_enable(temp_sensor));
+}
 
-    ESP_LOGI(TAG, "Read temperature");
-    int cnt = 20;
-    float tsens_value;
-    while (cnt--) {
-        ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_sensor, &tsens_value));
-        ESP_LOGI(TAG, "Temperature value %.02f ℃", tsens_value);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+void esp32_remotecar_1000ms()
+{
+    ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_sensor, &tsens_value));
+    ESP_LOGI(TAG, "Temperature value %.02f ℃", tsens_value);
+
+    if (led_st)
+    {
+        /* LED on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
     }
+    else
+    {
+        /* LED off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+    }
+
+    led_st = false
 }
 
 /* Main function */
 void app_main(void)
 {
-    printf("Hello world!\n");
-    xTaskCreate(&blink_task, "blink_task", 2048, NULL, 5, NULL); 
-    xTaskCreate(&Tsensor_task, "Tsensor_task", 2048, NULL, 5, NULL); 
+    xTaskCreate(&esp32_remotecar_init, "init task", 2048, NULL, 5, NULL); 
+
+    /* List 1000 ms periodic functions under this */
+    if(xTaskCheckForTimeOut(&x_timeout, &openTimeout) == pdTRUE)
+    {
+        esp32_remotecar_1000ms()
+        openTimeout = TIME_1000MS; /*reload the var*/
+    }
 }
