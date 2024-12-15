1b2c4288b9bc56e9c4535316f9e28a63d13c92b6
pkarthikbit <pkarthikbit@gmail.com>
2024-12-15 11:55:21 +0530

    ble_peripheral added

--- esp-idf/kpl_build.sh
+++ esp-idf/kpl_build.sh
@@ -1,6 +1,6 @@
 cd /home/priyanka/esp-idf/
 . ./export.sh
-cd /home/priyanka/Documents/esp32-remoteCar
+cd /home/priyanka/Documents/esp32-remoteCar/esp-idf
 idf.py set-target esp32-c6
 idf.py menuconfig
 idf.py build
--- esp-idf/kpl_flash.sh
+++ esp-idf/kpl_flash.sh
@@ -1,4 +1,4 @@
 cd /home/priyanka/esp-idf/
 . ./export.sh
-cd /home/priyanka/Documents/esp32-remoteCar
+cd /home/priyanka/Documents/esp32-remoteCar/esp-idf
 idf.py -p /dev/ttyACM0 flash
