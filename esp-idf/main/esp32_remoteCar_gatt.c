ec4108b7bf04b211cd709c1ff195188799a1e134
pkarthikbit <pkarthikbit@gmail.com>
2024-12-27 17:37:13 +0530

    use of utility func to print rx data

--- esp-idf/main/esp32_remoteCar_gatt.c
+++ esp-idf/main/esp32_remoteCar_gatt.c
@@ -6,6 +6,34 @@
 #include "esp32_remoteCar_inf.h"
 #include "esp32_remoteCar_priv.h"
 
+/**
+ * Utility function to log an array of bytes.
+ */
+void print_bytes(const uint8_t *bytes, int len)
+{
+    int i;
+
+    for (i = 0; i < len; i++) {
+        MODLOG_DFLT(DEBUG, "%s0x%02x", i != 0 ? ":" : "", bytes[i]);
+    }
+}
+
+void print_mbuf(const struct os_mbuf *om)
+{
+    int colon;
+
+    colon = 0;
+    while (om != NULL) {
+        if (colon) {
+            MODLOG_DFLT(DEBUG, ":");
+        } else {
+            colon = 1;
+        }
+        print_bytes(om->om_data, om->om_len);
+        om = SLIST_NEXT(om, om_next);
+    }
+}
+
 /* Callback function for custom service */
 static int  ble_svc_gatt_handler(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
@@ -15,7 +43,8 @@ static int  ble_svc_gatt_handler(uint16_t conn_handle, uint16_t attr_handle, str
         break;
 
     case BLE_GATT_ACCESS_OP_WRITE_CHR:
-        MODLOG_DFLT(INFO, "Data received in write event,ble_gatt_access_ctxt = %s", *ctxt);
+        MODLOG_DFLT(INFO, "Data received in write event,ble_gatt_access_ctxt");
+        print_mbuf(ctxt->om);
         break;
 
     default:
