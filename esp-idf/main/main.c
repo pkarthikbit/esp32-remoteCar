c6e795c9d116c4f9048a827f8ca4a011aa320b73
pkarthikbit <pkarthikbit@gmail.com>
2025-05-06 12:18:52 +0530

    Bugfixed the gpio invoke

--- esp-idf/main/main.c
+++ esp-idf/main/main.c
@@ -539,6 +539,8 @@ app_main(void)
 {
     int rc;
 
+    gpio_init();
+
     /* Initialize NVS — it is used to store PHY calibration data */
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
